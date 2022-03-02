281c651f7084b0955a57d7678743fa0a2c0d0486
irene22fg <irenefiguereo22@gmail.com>
2022-03-02 22:22:14 +0000

    componente

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function Componente(props){
+  return (
+    <div>
+      <h1>{props.titulo}</h1>
+      <div>{props.contenido}</div>
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Componente titulo="Hola" contenido="Que tal?"/>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
